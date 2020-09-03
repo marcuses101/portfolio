36889bcdceb2076001087efb54134fb2df1bf45d
marcuses101 <mnjconnolly@gmail.com>
2020-09-03 18:37:31 -0400

    added utility/ general edits

--- utility/sharp.js
+++ utility/sharp.js
@@ -4,7 +4,6 @@ const directory = '../img';
 
 fs.readdirSync(directory).forEach(file => {
   sharp(`${directory}/${file}`)
-    .resize(200)
     .toFile(`${directory}/${file}-small.jpg`, (err,info) => {
         if (err) {
             console.log(err)
